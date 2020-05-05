2634703b627c79bc07817f6c2d639d6f42671cdc
solbencas <sol.bencas@gmail.com>
2020-05-05 09:09:17 -0400

    adding logout functionality

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -3,10 +3,13 @@ import {
   createStackNavigator,
   createDrawerNavigator,
   createSwitchNavigator,
-  createAppContainer
+  createAppContainer,
+  DrawerItems,
 } from 'react-navigation';
-import { Platform } from 'react-native';
+import { Platform, SafeAreaView, Button, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useDispatch } from "react-redux";
+import * as authActions from "../store/actions/auth";
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
@@ -97,6 +100,21 @@ const ShopNavigator = createDrawerNavigator(
   {
     contentOptions: {
       activeTintColor: Colors.primary
+    }, 
+    contentComponent: props => {
+      const dispatch = useDispatch();
+
+      return (
+        <View style={{flex: 1, paddingTop: 20 }}>
+          <SafeAreaView forceInset={{top: "always", horizontal: "never"}}>
+            <DrawerItems {...props} />
+            <Button title="Logout" color={Colors.primary} onPress={() => {
+              dispatch(authActions.logout());
+              props.navigation.navigate("Auth");
+            }} />
+          </SafeAreaView>
+        </View>
+      )
     }
   }
 );
